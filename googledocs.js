f5443927f5c4e549f4d20afb8848332a38647236
Gergely Imreh <imrehg@gmail.com>
2011-08-22 12:33:35 +0800

    fix: names are not shown well

--- googledocs.js
+++ googledocs.js
@@ -343,7 +343,7 @@ GoogleDocs.prototype.buildFeedItemElement_ = function(template, feedItem) {
 
 
   Util.setAnchorHref(domFeedEntry, 'docs-entry-link', docUrl);
-  var modifiedBy = feedItem['gd$lastModifiedBy']['$t'];
+  var modifiedBy = feedItem['gd$lastModifiedBy']['name']['$t'] || 'unknown';
   Util.setChildHTML(domFeedEntry, 'docs-entry-byuser', 'by ' + modifiedBy);
 
   // Util.setName(domFeedEntry, 'feed-entry-item-id', itemId);
